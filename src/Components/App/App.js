ef17b0fe6272b9785256fa28317510e37feb74c6
winstonfarsoul <matthewjcollier@gmail.com>
2017-12-05 16:19:14 -0500

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { id: 1, name: 'A_name', artist: 'A_artist', album: 'A_album'},
+        { id: 2, name: 'B_name', artist: 'B_artist', album: 'B_album'},
+        { id: 3, name: 'C_name', artist: 'C_artist', album: 'C_album'},
+      ],
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* Add a SearchBar component */}
+          <div className="App-playlist">
+             <SearchResults searchResults={this.state.searchResults}/>
+             {/*Add a Playlist component  */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
